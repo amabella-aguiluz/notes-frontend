3cfa6033a4f2ab328e5df833dc539d0069984c38
laffingtafy <amabellaaguiluz1@gmail.com>
2026-04-20 20:11:56 +0800

    Chore: Set up Docker Fixed some inconsistencies in file names

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+console.log("BACKEND URL:", BACKEND_URL);
 
 export const api = axios.create({
     baseURL: BACKEND_URL,
